c056bbeec1fe3c94e76825ba85fca6eb0be5526c
sashaYanushevich <rewqsasha217@gmail.com>
2024-10-05 21:46:33 +0300

    add cors

--- app/main.py
+++ app/main.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI
 from app.api.v1.endpoints import users
 from app.core.config import settings
 import logging
+from fastapi.middleware.cors import CORSMiddleware
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 app = FastAPI(title="Telegram WebApp Game API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router, prefix=settings.API_V1_STR, tags=["users"])
 
